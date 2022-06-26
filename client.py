d621e6abe23d00f3347223d4c31aeed1df20a34f
Youssef Alsabban <youssefalsabban@gmail.com>
2022-06-26 19:49:24 +0200

    Added the textApp threads for both client and server

--- client.py
+++ client.py
@@ -62,8 +62,105 @@ def sendApp(network_queue, store_queue):
             s.recv_string()
         else :
             print("[FAILURE]")
+
+def textApp(input_queue, output_queue):
+    #This socket is used for the "connect" method
+    context = zmq.Context()
+    p3 = "tcp://" + HOST + ":" + PORT3
+    s = context.socket(zmq.REQ)
+        
+    def connect():
+        """Open a file for editing."""
+        txt_edit.config(state=tk.NORMAL) #Enables the textbox in the GUI
+        s.connect(p3)
+        s.send_string("MSG") #Sends to the server on thid socket "MSG" to request the file data on the server
+        text = s.recv_string() 
+        txt_edit.insert(tk.END, text) #the file writes the text received from the user on the textbox
+
+        
+ 
+
+    def save_file():
+        """Save the current file as a new file."""
+        filepath = asksaveasfilename(
+            defaultextension="txt",
+            filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+        )
+        if not filepath:
+            return
+        with open(filepath, "w") as output_file:
+            text = txt_edit.get(1.0, tk.END)
+            output_file.write(text)
+            window.update()
+
+    def recv_input():
+        
+        if(input_queue.empty()): #if there is no new data received
+            pass
+        else: 
+            data = input_queue.get(False)
+            message = data.message #take the message part of the received data and store in a variable
+            print(message)
+            myIndex = data.index #take the index part of the received data and store in a variable
+            
+            if(message == 13): #if the message part was the "enter" button
+                message = "\n"
+                txt_edit.insert(myIndex, message)
+            elif (message == 8): #if the message part was the "backspace" button
+                indList = myIndex.split(".") #3.9 ["3", "9"]
+                fRow = indList[0] #"3"
+                nRow = fRow
+                fCol = str(int(int(indList[1])-1)) 
+                #86.12  #86.13 
+                # a        b
+                nCol = indList[1]
+                fIndex = fRow + "." + fCol #index of deleted letter
+                nIndex = nRow + "." + nCol #the new index
+                txt_edit.delete(fIndex, nIndex)
+            else: #if the message is neither of the special characters
+                message = chr(message)
+                txt_edit.insert(myIndex, message)
+        window.after(1, recv_input)
+
+    def keydown(e): #on key event
+        cord = ord(e.char) #take the character of the key pressed
+        if (cord >= 0 & cord <= 127): #if it is an accepted key
+            dataToSend = TxtMsg(txt_edit.index(tk.INSERT), cord) #create an object to send with the index and the character to be sent
+            print (dataToSend.toString())
+            output_queue.put(dataToSend, False) #put the object into the push queue to send through the sendApp thread
+
+
+    window = tk.Tk()
+    window.title("Team 24 Client")
+    window.rowconfigure(0, minsize=800, weight=1)
+    window.columnconfigure(1, minsize=800, weight=1)
+
+    txt_edit = tk.Text(window, bg="#242424", fg="#FFFFFF", insertbackground="#CCCCCC")
+    fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+    btn_open = tk.Button(fr_buttons, text="Connect", command=connect)
+    btn_save = tk.Button(fr_buttons, text="Save As...", command=save_file)
+    
+
+
+
+    btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+    btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+    
+
+    fr_buttons.grid(row=0, column=0, sticky="ns")
+    txt_edit.grid(row=0, column=1, sticky="nsew")
+
+    
+    txt_edit.bind("<KeyPress>", keydown)
+    
+    window.after(1, recv_input)
+    
+    window.mainloop()
+
+textThread = threading.Thread(target=textApp, args=(localIntakeQueue, localPushQueue,))
 nSendThread = threading.Thread(target=sendApp, args=(localPushQueue, localStorageQueue,))
 nRecvThread = threading.Thread(target=recvApp, args=(localIntakeQueue, localStorageQueue,))
 
 nSendThread.start()
 nRecvThread.start()
+textThread.start()
--- serverThread.py
+++ serverThread.py
@@ -48,8 +48,87 @@ def sendApp(network_queue):
         s.send_pyobj(message)                       #send the message got from the queue
         print(f"[SENDING message from queue (Publisher)...]")
 
+def textApp(input_queue, output_queue):
+    
+
+    def open_file():
+        """Open a file for editing."""
+        
+        txt_edit.delete(1.0, tk.END)
+        file = open ("test.txt", "r")
+        text = file.read()
+        txt_edit.insert(tk.END, text)
+        window.title(f"Team 24 Server- test.txt")
+
+    def save_file():
+        """Save the current file as a new file."""
+
+        file = open ("test.txt", "w")
+        text = txt_edit.get(1.0, tk.END)
+        file.write(text)
+        window.update()
+        window.title(f"Team 24 Server- text.txt")
+
+    def recv_input():
+        
+        if(input_queue.empty()):
+            pass
+        else:
+            data = input_queue.get(False)
+            output_queue.put(data)
+            message = data.message
+            print(message)
+            myIndex = data.index
+            save_file()
+            
+            if(message == 13):
+                message = "\n"
+                txt_edit.insert(myIndex, message)
+                save_file()
+            elif (message == 8):
+                indList = myIndex.split(".") #3.9 ["3", "9"]
+                fRow = indList[0] #"3"
+                nRow = fRow
+                fCol = str(int(int(indList[1])-1)) 
+                #86.12  #86.13 
+                # a        b
+                nCol = indList[1]
+                fIndex = fRow + "." + fCol
+                nIndex = nRow + "." + nCol
+                txt_edit.delete(fIndex, nIndex)
+                save_file()
+            else:
+                message = chr(message)
+                txt_edit.insert(myIndex, message)
+                save_file()
+            print(txt_edit.index(tk.INSERT))
+        window.after(1, recv_input)
+    
+
+    window = tk.Tk()
+    window.title("Team 24 Server")
+    window.rowconfigure(0, minsize=800, weight=1)
+    window.columnconfigure(1, minsize=800, weight=1)
+
+    txt_edit = tk.Text(window, bg="#242424", fg="#FFFFFF", insertbackground="#CCCCCC")
+    fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+
+    btn_conn = tk.Button(fr_buttons, text="Start Server", command=open_file)
+
+    btn_conn.grid(row=0, column=0, sticky="ew", padx=5)
+    
+
+    fr_buttons.grid(row=0, column=0, sticky="ns")
+    txt_edit.grid(row=0, column=1, sticky="nsew")
+
+    window.after(1, recv_input)
+
+    window.mainloop()
+
+textThread = threading.Thread(target=textApp, args=(localRecvQueue, localPubQueue,))
 nRecvThread = threading.Thread(target=recvApp, args=(localRecvQueue,))
 nSendThread = threading.Thread(target=sendApp, args=(localPubQueue,))
 
 nRecvThread.start()
 nSendThread.start()
+textThread.start()
